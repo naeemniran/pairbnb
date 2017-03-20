82757627940ea0e941ab7baabf8b35695e29addd
kajer712 <naeemniran@gmail.com>
2017-03-20 15:11:22 +0800

    install Clearance gem for user management

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
+  include Clearance::Controller
   protect_from_forgery with: :exception
+
+  before_action :require_login
+
+  def index
+  	current_user.articles
+  end
+
 end
